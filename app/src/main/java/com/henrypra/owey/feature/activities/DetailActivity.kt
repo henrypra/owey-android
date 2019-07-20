1666c722d814004701af6ad128acaffdcc6eeffc
henrypra <16843315+henrypra@users.noreply.github.com>
2019-07-20 10:55:39 +0200

    Structure detail screen

---
+++ app/src/main/java/com/henrypra/owey/feature/activities/DetailActivity.kt
@@ -0,0 +1,41 @@
+package com.henrypra.owey.feature.activities
+
+import android.os.Bundle
+import androidx.room.Room
+import com.henrypra.owey.BaseActivity
+import com.henrypra.owey.R
+import com.henrypra.owey.feature.creation.CreationActionListener
+import com.henrypra.owey.feature.creation.CreationFragment
+import com.henrypra.owey.feature.creation.CreationPresenter
+import com.henrypra.owey.room.AppDatabase
+import com.henrypra.owey.utility.FragmentNavigation
+
+
+class DetailActivity : BaseActivity(),
+        CreationActionListener {
+    var appDatabase: AppDatabase? = null
+
+    private val fragmentNavigation: FragmentNavigation by lazy { FragmentNavigation(R.id.detail_fragment_container, this) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        appDatabase = Room.databaseBuilder(this, AppDatabase::class.java, DATABASE_NAME).fallbackToDestructiveMigration().build()
+
+        val intent = intent
+        val message = intent.getStringExtra("CURRENT_FRAGMENT")
+        if (message == "CREATION") {
+            loadCreationFragment()
+        }
+    }
+
+    private fun loadCreationFragment() {
+        val creationFragment = CreationFragment()
+        creationFragment.presenter = CreationPresenter(this, this, creationFragment, appDatabase)
+        fragmentNavigation.replaceFragment(creationFragment)
+    }
+
+    companion object {
+        private val DATABASE_NAME = "owey.db"
+    }
+}
--- app/src/main/java/com/henrypra/owey/feature/activities/MainActivity.kt
+++ app/src/main/java/com/henrypra/owey/feature/activities/MainActivity.kt
@@ -1,29 +1,56 @@
-package com.henrypra.owey.feature.main
+package com.henrypra.owey.feature.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.room.Room
 import com.henrypra.owey.BaseActivity
 import com.henrypra.owey.R
+import com.henrypra.owey.feature.main.MainActionListener
+import com.henrypra.owey.feature.main.MainFragment
+import com.henrypra.owey.feature.main.MainPresenter
 import com.henrypra.owey.room.AppDatabase
 import com.henrypra.owey.utility.FragmentNavigation
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : BaseActivity(),
-        MainActionListener {
-
+        MainActionListener,
+        View.OnClickListener {
     var appDatabase: AppDatabase? = null
 
     private val fragmentNavigation: FragmentNavigation by lazy { FragmentNavigation(R.id.main_fragment_container, this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         appDatabase = Room.databaseBuilder(this, AppDatabase::class.java, DATABASE_NAME).fallbackToDestructiveMigration().build()
         loadMainFragment()
+        initOnCLickListeners()
+    }
+
+    private fun initOnCLickListeners() {
+        fab?.setOnClickListener(this)
     }
 
     private fun loadMainFragment() {
         val mainFragment = MainFragment()
         mainFragment.presenter = MainPresenter(this, this, mainFragment, appDatabase)
         fragmentNavigation.replaceFragment(mainFragment)
+    }
+
+    private fun loadDetailActivity(fragment: String) {
+        intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra("CURRENT_FRAGMENT", fragment)
+        startActivity(intent)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            //todo add enum
+            R.id.fab -> loadDetailActivity("CREATION")
+
+        }
     }
 
     companion object {
